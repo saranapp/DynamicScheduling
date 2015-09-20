bcccafb6c3c2ae163ddace81708fb4c5ca7fc8ec
Saran Prasad Ambikapathy <asaranprasad@users.noreply.github.com>
2015-09-20 10:41:00 +0530

    Create IndexQueueMapper.java

---
+++ src/dataStructure/IndexQueueMapper.java
@@ -0,0 +1,11 @@
+package dataStructure;
+
+import java.util.HashMap;
+import java.util.PriorityQueue;
+import scheduler.TargetSystemRequest;
+
+
+public class IndexQueueMapper {
+
+	public static HashMap<String, PriorityQueue<TargetSystemRequest>> indexQueueMap = new HashMap<String, PriorityQueue<TargetSystemRequest>>();
+}
